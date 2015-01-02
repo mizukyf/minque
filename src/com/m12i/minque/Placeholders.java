ad7810d0bcdf8441e2885f68ac744676dd1b309d
mizuky fujitani <mizuki.fujitani@gmail.com>
2015-01-02 17:33:35 +0900

    Placeholdersにドキュメンテーションコメントを追加

--- src/com/m12i/minque/Placeholders.java
+++ src/com/m12i/minque/Placeholders.java
@@ -3,17 +3,30 @@ package com.m12i.minque;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * バインド変数のためのプレースホルダを管理するオブジェクト.
+ */
 final class Placeholders {
 	private final List<Expression> list = new ArrayList<Expression>();
 	
+	/**
+	 * プレースホルダへの参照を追加する.
+	 * @param e プレースホルダ
+	 */
 	void register(final Expression e) {
 		list.add(e);
 	}
-	
+	/**
+	 * プレースホルダの総数を返す.
+	 * @return プレースホルダの総数
+	 */
 	int amount() {
 		return list.size();
 	}
-	
+	/**
+	 * プレースホルダに変数をバインドする.
+	 * @param vars 変数
+	 */
 	void bind(final Object... vars) {
 		final int listSize = list.size();
 		if (vars.length != listSize) {
